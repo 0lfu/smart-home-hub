29aa074101960b47f644922e44330b89e70ddb19
Olfu <wojda.olaf@gmail.com>
2023-04-10 19:08:02 +0200

    draggable fix

--- scripts/draggable.js
+++ scripts/draggable.js
@@ -1,23 +1,41 @@
 $(document).ready(function() {
-    $(".component").draggable({
-        revert: true,
-        helper: "clone",
-        start: function() {
-            $(this).fadeTo("fast", 0.5); // reduce opacity while dragging
-        },
-        stop: function() {
-            $(this).fadeTo("fast", 1); // restore opacity after dragging
-        }
+    $(".component-header").mousedown(function() {
+        $(this).closest(".component").draggable({
+            revert: true,
+            helper: "clone",
+            start: function() {
+                $(this).fadeTo("fast", 0.5);
+            },
+            stop: function() {
+                $(this).fadeTo("fast", 1);
+            }
+        });
     });
-    
+
     $(".column").droppable({
         accept: ".component",
         drop: function(event, ui) {
-            $(this).append(ui.draggable);
+            var droppedComponent = ui.draggable;
+            var newColumn = $(this);
+            droppedComponent.draggable("destroy");
+            droppedComponent.draggable({
+                revert: true,
+                helper: "clone",
+                containment: newColumn,
+                handle: ".component-header",
+                start: function() {
+                    $(this).fadeTo("fast", 0.5);
+                },
+                stop: function() {
+                    $(this).fadeTo("fast", 1);
+                }
+            });
+
+            newColumn.append(droppedComponent);
         }
     }).sortable({
         axis: "y",
-        handle: ".component",
+        handle: ".component-header",
         containment: "parent",
         tolerance: "pointer"
     });
