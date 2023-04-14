5c04211a1ad1b884c14d1454deb4b2f504a68cb5
Olfu <wojda.olaf@gmail.com>
2023-04-15 00:30:18 +0200

    added date

--- components/clock/clock.js
+++ components/clock/clock.js
@@ -1,12 +1,19 @@
 const clockContainer = $('#clock .component-body').get(0);
 
 if (clockContainer) {
-  setInterval(() => {
+  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const updateClock = () => {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    clockContainer.innerText = timeString;
-  }, 1000);
+    const [day, weekday, month, year, hours, minutes, seconds] = [      now.getDate().toString().padStart(2, "0"),      weekdays[now.getDay()],
+      months[now.getMonth()],
+      now.getFullYear(),
+      now.getHours().toString().padStart(2, "0"),
+      now.getMinutes().toString().padStart(2, "0"),
+      now.getSeconds().toString().padStart(2, "0")
+    ];
+    clockContainer.innerHTML = `${hours}:${minutes}:${seconds}<span class='date-time'>${weekday}, ${day} ${month} ${year}</span>`;
+  };
+  updateClock();
+  setInterval(updateClock, 1000);
 }
